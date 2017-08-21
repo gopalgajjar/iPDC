9fc58197721e5ccc2f87955ff55ca0b5fada3eea
Gopal Gajjar <gopalgajjar@ieee.org>
2017-08-21 23:38:02 +0530

    added DBServer.desktop and runDBServer files.

--- DBServer/creatDirStructure.sh
+++ DBServer/creatDirStructure.sh
@@ -4,18 +4,18 @@
 
 creatDirAndMove ()
 {
-IFS=$2
-arr2=$1
-let k=0
-for x in $arr2
-do
-    let k=k+1
-    b[k]=$x
-done
-d=${b[k-4]}/${b[k-3]}/${b[k-2]}/${b[$k-1]}
-mkdir -p ${b[k-4]}/${b[k-3]}/${b[k-2]}/${b[$k-1]} 
-unset IFS
-mv $1 $d
+    IFS=$2
+    arr2=$1
+    let k=0
+    for x in $arr2
+    do
+        let k=k+1
+        b[k]=$x
+    done
+    d=${b[k-4]}/${b[k-3]}/${b[k-2]}/${b[$k-1]}
+    mkdir -p ${b[k-4]}/${b[k-3]}/${b[k-2]}/${b[$k-1]} 
+    unset IFS
+    mv $1 $d
 }
 
 for file in $@ 
